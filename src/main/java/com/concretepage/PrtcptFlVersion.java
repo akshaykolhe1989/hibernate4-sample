933038a97040f0a59e49fdadbe633fe5410ad530
Akshay <akshaykolhe1809@gmail.com>
2018-03-05 00:23:11 +0530

    added new columns

--- src/main/java/com/concretepage/PrtcptFlVersion.java
+++ src/main/java/com/concretepage/PrtcptFlVersion.java
@@ -8,24 +8,31 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="PRTCPT_FL_VERSION")
-public class PrtcptFlVersion implements Serializable{ 
+@Table(name = "PRTCPT_FL_VERSION")
+public class PrtcptFlVersion implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="PRTCPT_FL_VERSION_ID")
-    private int prtcptFlVersionId;  
-	
-	@Column(name="PRTCPT_ID")
+	@Column(name = "PRTCPT_FL_VERSION_ID")
+	private int prtcptFlVersionId;
+
+	@Column(name = "PRTCPT_ID")
 	private int prtcptId;
-	
+
 	@ManyToOne(optional = true)
-    @JoinColumn(name = "PRTCPT_ID", insertable=false, updatable=false)
-    private PrtcptProf prtcptProf;
+	@JoinColumn(name = "PRTCPT_ID", insertable = false, updatable = false)
+	private PrtcptProf prtcptProf;
+
+	@Column(name = "EFF_DATE")
+	private java.sql.Date effDate;
+
+	@Column(name = "EXP_DATE")
+	private java.sql.Date expDate;
 
 	public int getPrtcptFlVersionId() {
 		return prtcptFlVersionId;
@@ -51,14 +58,28 @@ public class PrtcptFlVersion implements Serializable{
 		this.prtcptProf = prtcptProf;
 	}
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
+	public java.sql.Date getEffDate() {
+		return effDate;
+	}
+
+	public void setEffDate(java.sql.Date effDate) {
+		this.effDate = effDate;
+	}
+
+	public java.sql.Date getExpDate() {
+		return expDate;
+	}
+
+	public void setExpDate(java.sql.Date expDate) {
+		this.expDate = expDate;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((effDate == null) ? 0 : effDate.hashCode());
+		result = prime * result + ((expDate == null) ? 0 : expDate.hashCode());
 		result = prime * result + prtcptFlVersionId;
 		result = prime * result + prtcptId;
 		result = prime * result + ((prtcptProf == null) ? 0 : prtcptProf.hashCode());
@@ -74,6 +95,16 @@ public class PrtcptFlVersion implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		PrtcptFlVersion other = (PrtcptFlVersion) obj;
+		if (effDate == null) {
+			if (other.effDate != null)
+				return false;
+		} else if (!effDate.equals(other.effDate))
+			return false;
+		if (expDate == null) {
+			if (other.expDate != null)
+				return false;
+		} else if (!expDate.equals(other.expDate))
+			return false;
 		if (prtcptFlVersionId != other.prtcptFlVersionId)
 			return false;
 		if (prtcptId != other.prtcptId)
@@ -89,11 +120,7 @@ public class PrtcptFlVersion implements Serializable{
 	@Override
 	public String toString() {
 		return "PrtcptFlVersion [prtcptFlVersionId=" + prtcptFlVersionId + ", prtcptId=" + prtcptId + ", prtcptProf="
-				+ prtcptProf + "]";
+				+ prtcptProf + ", effDate=" + effDate + ", expDate=" + expDate + "]";
 	}
-
-	
-	
-	
 
 }
